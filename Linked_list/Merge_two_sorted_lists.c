ccbd6290dd5cb1f176e0df8952e1108e32991bd7
HaitamJBILAT01 <haitemjbilat@gmail.com>
2024-12-29 23:26:32 +0100

    First leetcode solved exercice

---
+++ Linked_list/Merge_two_sorted_lists.c
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+
+
+      
+     struct ListNode* dummyNode = (struct ListNode*) malloc(sizeof(struct ListNode));
+
+                if(dummyNode == NULL){
+                    printf("Allocation Failed");
+                    exit(1);
+                }
+               
+               dummyNode->val = -1;
+               dummyNode->next = NULL;
+
+
+               struct ListNode *current1, *current2, *current3;
+
+                     current1 = list1;
+                     current2 = list2;
+                     current3 = dummyNode;
+
+
+                     while(current1 != NULL && current2 != NULL){
+
+
+                                 if(current1->val <= current2->val){
+
+                                    current3->next = current1;
+                                    current1 = current1->next;
+                                    current3 = current3->next;
+                                 }
+
+                                 else{
+
+                                   current3->next = current2;
+                                   current2 = current2->next;
+                                   current3 = current3->next;
+                                 }
+                        
+                     }
+
+                     if(current1 == NULL)
+                        current3->next = current2;
+                        else if(current2 == NULL)
+                         current3->next = current1;
+
+
+          return dummyNode->next;
+}
